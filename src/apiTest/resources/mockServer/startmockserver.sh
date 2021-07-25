dfd2f9ad4bca688dfad1a601f4ae6d8c39dfa570
Michal Malík <97malimi@gmail.com>
2021-07-25 18:14:32 +0200

    action

--- src/apiTest/resources/mockServer/startmockserver.sh
+++ src/apiTest/resources/mockServer/startmockserver.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar mockserver-netty-5.11.2-jar-with-dependencies.jar -serverPort 8080
+java -jar src/apiTest/resources/mockServer/mockserver-netty-5.11.2-jar-with-dependencies.jar -serverPort 8080
