8465b668a71f5d6d7387592393f89fa5e246ed02
Michal Malík <97malimi@gmail.com>
2021-08-01 19:04:40 +0200

    0.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 import org.jetbrains.intellij.tasks.PatchPluginXmlTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import java.net.URI
 
 plugins {
   id("org.jetbrains.intellij") version "0.6.5"
-  kotlin("jvm") version "1.4.30"
+  kotlin("jvm") version "1.4.32"
   java
 }
 
@@ -12,26 +11,26 @@ apply(plugin = "kotlin")
 apply(plugin = "org.jetbrains.intellij")
 
 group = "eu.ibagroup"
-version = "0.2"
+version = "0.4.1"
 
 repositories {
   mavenCentral()
+  //temporary solution to enable ZOWE community work
+  flatDir {
+    dir("libs")
+  }
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_11
-  targetCompatibility = JavaVersion.VERSION_11
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.withType(KotlinCompile::class).all {
   kotlinOptions {
-    jvmTarget = JavaVersion.VERSION_11.toString()
-    languageVersion = org.jetbrains.kotlin.config.LanguageVersion.LATEST_STABLE.versionString
+    jvmTarget = "1.8"
+    languageVersion = "1.4"
   }
-}
-
-tasks.buildSearchableOptions {
-  enabled = false
 }
 
 dependencies {
@@ -42,27 +41,28 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.30")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
   implementation("org.jgrapht:jgrapht-core:1.5.0")
-  implementation("eu.ibagroup:r2z:1.0.2")
+  implementation("eu.ibagroup:r2z:1.0.13")
+  implementation("com.segment.analytics.java:analytics:+")
   testImplementation("junit", "junit", "4.12")
 }
 
 intellij {
-  version = "2020.3"
+  version = "2021.1.3"
 }
-
-
 
 tasks.getByName<PatchPluginXmlTask>("patchPluginXml") {
   sinceBuild("203.5981")
   untilBuild("211.*")
   changeNotes(
     """
-      In version 0.2 we added:<br/>
+      In version 0.4.1 we added:<br/>
       <ul>
-        <li>Binary and text modes added for USS files and data sets</li>
-        <li>Error messages are improved a bit</li>
-        <li>Possibility to add a DS Mask right from File Explorer's context menu</li>
-        <li>Small UI fixes</li>
+        <li>Pride logo to support LGBTQIA+ community. Peace, love, pride</li>
+        <li>Job submission by the right click on files in the File Explorer</li>
+        <li>Move and Copy operations are available for USS files and directories</li>
+        <li>Editing Working Sets is now accessible by the right click on the Working Set in the File Explorer</li>
+        <li>Tracking analytics events is now enabled with corresponding Privacy Policy</li>
+        <li>Small UI fixes.</li>
       </ul>"""
   )
 }
--- src/main/kotlin/eu/ibagroup/formainframe/api/ZosmfApiImpl.kt
+++ src/main/kotlin/eu/ibagroup/formainframe/api/ZosmfApiImpl.kt
@@ -6,11 +6,21 @@ import eu.ibagroup.formainframe.config.connect.ConnectionConfig
 import eu.ibagroup.formainframe.config.connect.UrlConnection
 import eu.ibagroup.formainframe.utils.crudable.getByForeignKey
 import eu.ibagroup.r2z.buildApi
+import okhttp3.ConnectionPool
+import okhttp3.ConnectionSpec
+import okhttp3.Dispatcher
 import okhttp3.OkHttpClient
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
+import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
-class ZosmfApiImpl : ZoweApi {
+class ZosmfApiImpl : ZosmfApi {
 
   private data class ZosmfUrl(val url: String, val isAllowSelfSigned: Boolean)
 
@@ -51,7 +61,6 @@ class ZosmfApiImpl : ZoweApi {
 private val gsonFactory = GsonConverterFactory.create(GsonBuilder().create())
 private val scalarsConverterFactory = ScalarsConverterFactory.create()
 
-/*
 private fun OkHttpClient.Builder.addThreadPool(): OkHttpClient.Builder {
   readTimeout(5, TimeUnit.MINUTES)
   connectTimeout(5, TimeUnit.MINUTES)
@@ -62,26 +71,32 @@ private fun OkHttpClient.Builder.addThreadPool(): OkHttpClient.Builder {
   })
   return this
 }
-*/
 
-/*
 val unsafeOkHttpClient by lazy { buildUnsafeClient() }
-val safeOkHttpClient by lazy {
+val safeOkHttpClient: OkHttpClient by lazy {
   OkHttpClient.Builder()
-    .addThreadPool()
+    .setupClient()
     .build()
 }
-*/
+
+private fun OkHttpClient.Builder.setupClient(): OkHttpClient.Builder {
+  return addThreadPool()
+    .addInterceptor {
+      it.request().newBuilder().addHeader("X-CSRF-ZOSMF-HEADER", "").build().let { request ->
+        it.proceed(request)
+      }
+    }.connectionSpecs(mutableListOf(ConnectionSpec.MODERN_TLS, ConnectionSpec.COMPATIBLE_TLS))
+}
 
 private fun getOkHttpClient(isAllowSelfSigned: Boolean): OkHttpClient {
   return if (isAllowSelfSigned) {
-    ZoweApi.unsafeOkHttpClient
+    unsafeOkHttpClient
   } else {
-    ZoweApi.safeOkHttpClient
+    safeOkHttpClient
   }
 }
 
-/*private fun buildUnsafeClient(): OkHttpClient {
+private fun buildUnsafeClient(): OkHttpClient {
   return try {
     val trustAllCerts: Array<TrustManager> = arrayOf(
       object : X509TrustManager {
@@ -110,9 +125,9 @@ private fun getOkHttpClient(isAllowSelfSigned: Boolean): OkHttpClient {
     val builder = OkHttpClient.Builder()
     builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
     builder.hostnameVerifier { _, _ -> true }
-    builder.addThreadPool()
+    builder.setupClient()
     builder.build()
   } catch (e: Exception) {
     throw RuntimeException(e)
   }
-}*/
+}
