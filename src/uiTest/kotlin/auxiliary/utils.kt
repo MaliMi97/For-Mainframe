977fcee492b9efe8cef83030ea35b28f2d7ea605
Michal Malík <97malimi@gmail.com>
2021-08-19 02:17:45 +0200

    60

--- src/uiTest/kotlin/auxiliary/utils.kt
+++ src/uiTest/kotlin/auxiliary/utils.kt
@@ -6,16 +6,24 @@ import com.intellij.remoterobot.utils.waitFor
 import java.time.Duration
 
 fun CommonContainerFixture.clickButton(text: String) {
-    button(text).clickWhenEnabled()
+    val button = button(text)
+    waitFor(Duration.ofSeconds(60)) {
+        button.isEnabled()
+    }
+    button.click()
 }
 
 fun CommonContainerFixture.clickButton(locator: Locator) {
-    button(locator).clickWhenEnabled()
+    val button = button(locator)
+    waitFor(Duration.ofSeconds(60)) {
+        button.isEnabled()
+    }
+    button.click()
 }
 
 fun CommonContainerFixture.clickActionButton(locator: Locator) {
     val button = actionButton(locator)
-    waitFor(Duration.ofSeconds(5)) {
+    waitFor(Duration.ofSeconds(60)) {
         button.isEnabled()
     }
     button.click()
